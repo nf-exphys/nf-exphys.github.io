9c72b267e6a5f604971cf2cf69ed0459e174e38e
nf-exphys <68116178+nf-exphys@users.noreply.github.com>
2025-04-30 22:07:52 +0000

    Deploying to gh-pages from @ nf-exphys/nf-exphys.github.io@e7fda9594a7ba36adb1e3d3253273a0a5a4d376d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -45,8 +45,8 @@ ninja.data = [{
             window.location.href = "/cv/";
           },
         },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          title: "Teaching",
+          description: "This page describes my teaching experience.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
