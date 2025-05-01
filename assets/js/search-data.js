43678161a2b81b55f588fffade475b4113daaa8c
nf-exphys <68116178+nf-exphys@users.noreply.github.com>
2025-05-01 00:19:47 +0000

    Deploying to gh-pages from @ nf-exphys/nf-exphys.github.io@eaa2b2879a2879996367af88c6d1b88e68c8668e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -403,9 +403,9 @@ ninja.data = [{
           description: "An ongoing research study",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-predicting-vat-using-machine-learning",
+          title: 'Predicting VAT using Machine Learning',
+          description: "Ongoing secondary data analysis",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
